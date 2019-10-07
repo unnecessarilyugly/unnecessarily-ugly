2722a30ecc7b2031a0edb287cbfdc48953abf2f1
og.skinman <cindyhsu315@gmail.com>
2019-10-07 00:46:48 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var elem = document.querySelector('.grid-slider-wrap');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
